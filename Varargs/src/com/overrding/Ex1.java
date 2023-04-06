9cec1b547e5111a2ca6e85d6a15ab4ae990fa35e
Milind <milindkondekar55@gmail.com>
2023-04-06 09:23:28 +0530

    06-04-23

---
+++ Varargs/src/com/overrding/Ex1.java
@@ -0,0 +1,27 @@
+package com.overrding;
+
+class A 
+{
+	public void varargs(int ...a)
+	{
+
+	}
+}
+
+
+public class Ex1 extends A {
+
+	public void varargs(int ...a)
+	{
+
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
